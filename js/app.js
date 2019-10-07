7354b27a1ca186604dcbfdb0d6ad3d200dc4c17f
Pedro M <membrep@gmail.com>
2019-10-07 16:15:57 -0700

    added animation

---
+++ js/app.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    // $("#writing-text").css({"background-color": "#C8412B"})
+    // var typed = new Typed("#writing-text", {
+    //     strings: [
+    //         "am a Full-Stack Web Developer.", "also teach programming to people.", "solve problems."
+    //     ],
+    //     // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+    //     stringsElement: null,
+    //     // typing speed
+    //     typeSpeed: 55,
+    //     backSpeed: 55,
+    //     showCursor: false,
+    //     contentType: 'text',
+    //     callback: function() {
+    //         $("#writing-text").css({"color": "#fff", "background-color": "#C8412B"});
+    //     },
+    //     preStringTyped: function() {},
+    //     onStringTyped: function() {}
+    // })
+    // return {
+    //     typeAnimation: typeAnimation
+    // }
+    // $("#writing-text").css("background-color", "#C8412B");
+});
